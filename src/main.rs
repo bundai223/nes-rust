42f2b5383277fd0fd3e01c27a6e0a53ce82559ff
bundai223 <bundai223@gmail.com>
2018-03-13 00:33:04 +0900

    ファイル読むまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::{fs, mem};
+use std::io::{BufReader, Read};
+
 fn main() {
+    let mut f = BufReader::new(fs::File::open("rom/sample1/sample1.nes").unwrap());
+    let mut b: [ u8; 40976] = unsafe { mem::uninitialized() };
+
+    f.read_exact(&mut b).unwrap();
+
     println!("Hello, world!");
 }
