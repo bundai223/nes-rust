a2847a719b17bdf53ee43a06bda55c7ae002b795
bundai223 <bundai223@gmail.com>
2018-03-13 22:37:32 +0900

    header部分のdumpまで

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::{fs, mem};
 use std::io::{BufReader, Read};
 
+const NES_HEADER_SIZE: usize = 4;
+const NES_PROGRAM_ROM_SIZE: usize = 16 * 1024 * 1024;
+const NES_CHARACTER_ROM_SIZE: usize = 8 * 1024 * 1024;
+
 fn main() {
     let mut f = BufReader::new(fs::File::open("rom/sample1/sample1.nes").unwrap());
-    let mut b: [ u8; 40976] = unsafe { mem::uninitialized() };
+    let mut header: [ u8; NES_HEADER_SIZE] = unsafe { mem::uninitialized() };
+    // let mut programrom: [ u8; NES_PROGRAM_ROM_SIZE] = unsafe { mem::uninitialized() };
+    // let mut charactorrom: [ u8; NES_CHARACTER_ROM_SIZE] = unsafe { mem::uninitialized() };
 
-    f.read_exact(&mut b).unwrap();
+    f.read_exact(&mut header).unwrap();
 
-    println!("Hello, world!");
+    println!("Header Dump");
+    for i in 0..NES_HEADER_SIZE {
+      print!("{:X} ", header[i]);
+    }
+    println!("");
 }
