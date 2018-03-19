ff6415b6315d3e8bac293d55ff268861a841b40a
bundai223 <bundai223@gmail.com>
2018-03-20 07:03:11 +0900

    rom一括readしたいがstackoverflow

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ const NES_HEADER_SIZE: usize = 4;
 const NES_PROGRAM_ROM_SIZE: usize = 16 * 1024 * 1024;
 const NES_CHARACTER_ROM_SIZE: usize = 8 * 1024 * 1024;
 
+// struct Rom {
+//     header:  
+// };
+
 fn main() {
     let mut f = BufReader::new(fs::File::open("rom/sample1/sample1.nes").unwrap());
-    let mut header: [ u8; NES_HEADER_SIZE] = unsafe { mem::uninitialized() };
-    // let mut programrom: [ u8; NES_PROGRAM_ROM_SIZE] = unsafe { mem::uninitialized() };
-    // let mut charactorrom: [ u8; NES_CHARACTER_ROM_SIZE] = unsafe { mem::uninitialized() };
+    let mut rom: [ u8; NES_HEADER_SIZE + NES_PROGRAM_ROM_SIZE + NES_CHARACTER_ROM_SIZE ] = unsafe { mem::uninitialized() };
 
-    f.read_exact(&mut header).unwrap();
+    f.read_exact(&mut rom).unwrap();
 
     println!("Header Dump");
     for i in 0..NES_HEADER_SIZE {
-      print!("{:X} ", header[i]);
+      print!("{:X} ", rom[i]);
     }
     println!("");
 }
